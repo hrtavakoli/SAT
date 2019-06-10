d181a4fc69dc0e2a46fcde0ebd43377b7def02a9
hrtavakoli <h.rtavakoli@yahoo.com>
2019-06-10 16:53:33 +0300

    initiate mlnet structure

--- models/mlnet.py
+++ models/mlnet.py
@@ -1,4 +1,21 @@
 
 # Implementation of MLNet model by Cornia et al.
 
- 
+import torch
+import torch.nn as nn
+
+from torchvision.models import vgg16
+
+
+class Model(nn.Module):
+    # mlnet model
+
+    def __init__(self):
+         super(Model, self).__init__()
+
+         
+
+
+    def forward(self, inputs):
+        pass
+
