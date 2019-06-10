0ce86bafa19d701d03da5467c5d9c9ee1ba0da8d
hrtavakoli <h.rtavakoli@yahoo.com>
2019-06-10 17:11:58 +0300

    ed

--- models/mlnet.py
+++ models/mlnet.py
@@ -11,9 +11,8 @@ class Model(nn.Module):
     # mlnet model
 
     def __init__(self):
-         super(Model, self).__init__()
-
-         
+        super(Model, self).__init__()
+        self.features = nn.ModuleList(list(vgg16(pretrained=True).features)[:-1])
 
 
     def forward(self, inputs):
