f95b2041f11aea58878ddb06488b9b0adb7464cd
hrtavakoli <h.rtavakoli@yahoo.com>
2019-06-28 16:29:57 +0300

    added cycle

--- models/reconv.py
+++ models/reconv.py
@@ -8,26 +8,51 @@ Implementation of various recurrent convolution architectures
 import torch
 import torch.nn as nn
 
-class AttnConvLSTM(nn.Module):
-    """
-        Attentive Conv LSTM
-    """
+class ConvLSTMCell(nn.Module):
 
-    def __init__(self, input_channels, out_channels, kernel_size, hidden_size, return_sequences=False, bias=True):
+    def __init__(self, input_channels, hidden_channels, kernel_size, spatial_size):
         """
 
         :param input_channels: number of channels in the input tensor
-        :param out_channels: number of expected output channels
+        :param hidden_channels: number of expected output channels to the hidden space
         :param kernel_size: size of the convolution kernel
-        :param hidden_size:
-        :param bias:
+        :param spatial_size:
         """
-        super(AttnConvLSTM, self).__init__()
+        super(ConvLSTMCell, self).__init__()
 
         self.input_channels = input_channels
-        self.out_channels = out_channels
-        self.hidden_size = hidden_size
+        self.hidden_channels = hidden_channels
         self.kernel_size = kernel_size
 
-        self.conv_cell = nn.conv2d(input_channels=self.input_channels, out_channels=self.out_channels)
+        # define input gate weights
+        self.W_xi = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=True)
+        self.W_hi = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=False)
 
+        self.W_ci = nn.Parameter(torch.zeros(hidden_channels, spatial_size[0], spatial_size[1]))
+
+        # define forget gate weights
+        self.W_xf = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=True)
+        self.W_hf = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=False)
+
+        self.W_cf = nn.Parameter(torch.zeros(hidden_channels, spatial_size[0], spatial_size[1]))
+
+        # define output gate weights
+        self.W_xo = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=True)
+        self.W_ho = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=False)
+
+        self.W_co = nn.Parameter(torch.zeros(hidden_channels, spatial_size[0], spatial_size[1]))
+
+        # define cell weights
+        self.W_xc = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=True)
+        self.W_hc = nn.Conv2d(input_channels, hidden_channels, kernel_size=kernel_size, stride=1, padding=0, bias=False)
+
+
+    def _init_weights_(self):
+
+        @pass
+
+
+    def forward(self, x, h, c):
+        ig = nn.Sigmoid(
+                self.W_xc(x) + self.W_hi(x) +  torch.mul(self.W_ci.expand_as(c), c)
+        )
