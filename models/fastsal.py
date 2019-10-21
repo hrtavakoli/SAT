58d547c3469fa47ca1007ee59d250ec1e4a86db9
Xiao Shanghua <hallazie@gmail.com>
2019-10-22 02:03:24 +0800

    change fastsal to single subnet, add 2 layers and remove batchnorm at last layer

--- models/fastsal.py
+++ models/fastsal.py
@@ -20,14 +20,14 @@ class InvertedResidual(nn.Module):
         super(InvertedResidual, self).__init__()
         self.use_res = c_in == c_out
         self.conv = nn.Sequential(
-            nn.Conv2d(c_in, exp, kernel_size=1, stride=1, padding=0, groups=1, bias=False),
+            nn.Conv2d(c_in, exp, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(exp),
             nn.ReLU6(inplace=INPLACE),
             nn.Conv2d(exp, exp, kernel_size=3, stride=1, padding=1, groups=exp, bias=False),
             nn.BatchNorm2d(exp),
             nn.ReLU6(inplace=INPLACE),
-            nn.Conv2d(exp, c_out, kernel_size=1, stride=1, padding=0, groups=1, bias=False),
-            nn.BatchNorm2d(c_out),
+            nn.Conv2d(exp, c_out, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.ReLU6(inplace=INPLACE)
         )
 
     def forward(self, x):
@@ -42,39 +42,18 @@ class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.fine_net = self.SubNet()
-        self.coarse_net = self.SubNet()
 
     def forward(self, fine_batch):
 
         fine = self.fine_net(fine_batch)
-        coarse = F.interpolate(self.coarse_net(F.interpolate(fine_batch, scale_factor=0.5)), scale_factor=2)
-        mul = coarse * fine
-        return mul
-
-    # def SubNet(self):
-    #     return nn.Sequential(
-    #         self.Conv(3, 32, 3, 1),
-    #         self.Pool('max'),
-    #         self.ConvDWS(32, 16, 64),
-    #         self.ConvDWS(16, 16, 64),
-    #         self.Pool('max'),
-    #         self.ConvDWS(16, 24, 96),
-    #         self.ConvDWS(24, 24, 96),
-    #         self.ConvDWS(24, 24, 96),
-    #         self.Pool('max'),
-    #         self.ConvDWS(24, 32, 128),
-    #         self.ConvDWS(32, 32, 128),
-    #         self.ConvDWS(32, 32, 128),
-    #         self.ConvDWS(32, 32, 128),
-    #         self.ConvDWS(32, 64, 256),
-    #         self.ConvDWS(64, 64, 256),
-    #         self.ConvDWS(64, 128, 512),
-    #         self.Conv(128, 1, 1, 0)
-    #     )
+        # coarse = F.interpolate(self.coarse_net(F.interpolate(fine_batch, scale_factor=0.5)), scale_factor=2)
+        # mul = coarse * fine
+        return fine
 
     def SubNet(self):
         return nn.Sequential(
             self.Conv(3, 32, 3, 1),
+            self.Conv(32, 32, 3, 1),
             self.Pool('max'),
             InvertedResidual(32, 16, 64),
             InvertedResidual(16, 16, 64),
@@ -90,83 +69,11 @@ class Model(nn.Module):
             InvertedResidual(32, 64, 256),
             InvertedResidual(64, 64, 256),
             InvertedResidual(64, 128, 512),
-            self.Conv(128, 1, 1, 0)
+            InvertedResidual(128, 128, 512),
+            nn.Conv2d(128, 1, kernel_size=1),
+            nn.ReLU6(inplace=INPLACE)
         )
 
-    # def OverfitNet(self):
-    #
-    #     return nn.Sequential(
-    #         self.Conv(3, 32, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(32, 16, 3, 1),
-    #         self.Conv(16, 16, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(16, 24, 3, 1),
-    #         self.Conv(24, 24, 3, 1),
-    #         self.Conv(24, 24, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(24, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 64, 3, 1),
-    #         self.Conv(64, 64, 3, 1),
-    #         self.Conv(64, 128, 3, 1),
-    #         self.Conv(128, 1, 1, 0)
-    #     )
-
-    # def TinyNet(self):
-    #     return nn.Sequential(
-    #         self.Conv(3, 32, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(32, 16, 3, 1),
-    #         self.Conv(16, 16, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(16, 24, 3, 1),
-    #         self.Conv(24, 24, 3, 1),
-    #         self.Conv(24, 24, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(24, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 64, 3, 1),
-    #         self.Conv(64, 64, 3, 1),
-    #         self.Conv(64, 128, 3, 1),
-    #         self.Conv(128, 1, 1, 0)
-    #     )
-    #
-    # def MicroNet(self):
-    #     return nn.Sequential(
-    #         self.Conv(3, 32, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(32, 16, 3, 1),
-    #         self.Conv(16, 16, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(16, 24, 3, 1),
-    #         self.Conv(24, 24, 3, 1),
-    #         self.Conv(24, 24, 3, 1),
-    #         self.Pool('max'),
-    #         self.Conv(24, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 32, 3, 1),
-    #         self.Conv(32, 1, 1, 0)
-    #     )
-
-    # def ConvDWS(self, c_in, c_out, exp):
-    #     return nn.Sequential(
-    #         nn.Conv2d(c_in, exp, kernel_size=1, stride=1, padding=0, groups=1, bias=False),
-    #         nn.BatchNorm2d(exp),
-    #         nn.ReLU6(inplace=INPLACE),
-    #         nn.Conv2d(exp, exp, kernel_size=3, stride=1, padding=1, groups=exp, bias=False),
-    #         nn.BatchNorm2d(exp),
-    #         nn.ReLU6(inplace=INPLACE),
-    #         nn.Conv2d(exp, c_out, kernel_size=1, stride=1, padding=0, groups=1, bias=False),
-    #         nn.BatchNorm2d(c_out),
-    #         # nn.ReLU6(inplace=INPLACE)
-    #     )
-    #
     def Conv(self, c_in, c_out, ksize, pad):
         return nn.Sequential(
             nn.Conv2d(c_in, c_out, kernel_size=ksize, stride=1, padding=pad, bias=False),
@@ -180,7 +87,7 @@ class Model(nn.Module):
 
 
 if __name__ == "__main__":
-    sample_input = torch.zeros(1, 3, 512, 640).cuda()
+    sample_input = torch.zeros(1, 3, 256, 320).cuda()
     model = Model().cuda()
     a = model(sample_input)
     print(a.shape)
