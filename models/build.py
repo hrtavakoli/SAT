921b39160aa8d584a231c726ea15df3098e9b6b1
hallazie <hallazie@outlook.com>
2019-12-10 11:31:05 +0800

    rollback some modules

--- models/build.py
+++ models/build.py
@@ -5,7 +5,7 @@ Get the model of interest
 @author: Hamed R. Tavakoli
 '''
 
-MODEL_NAME = ['deepgaze2', 'mlnet', 'resnetprune', 'salicon', 'samres', 'deepfix', 'fastsal', 'mobilesal']
+MODEL_NAME = ['deepgaze2', 'mlnet', 'resnetsal', 'salicon', 'samres', 'deepfix', 'fastsal', 'mobilesal']
 
 
 class ModelConfig:
